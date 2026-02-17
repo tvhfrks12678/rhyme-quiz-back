a6bfe259109e1d60d85b44a0ad02e9e37b4edf2f
mitsumura-shouichirou-mac <tvhfrks12678@gmail.com>
2026-02-17 12:32:39 +0900

    fix(deploy): export Elysia.fetch for Vercel WebStandard runtime

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { Elysia } from "elysia";
+import { Elysia, t,  } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia() 
+    .get('/', () => 'Hello Vercel Function')
+    .post('/', ({ body }) => body, {
+        body: t.Object({
+            name: t.String()
+        })
+    })
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+export default app;
+
+// const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+// console.log(
+//   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+// );
+
